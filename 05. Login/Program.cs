f1e7815f30b5dd34192ae909d504d68725f05d0e
Boyan Venkov <boyanvenkov@yahoo.com>
2026-03-15 10:35:54 +0200

    Added a New Project

---
+++ 05. Login/Program.cs
@@ -0,0 +1,29 @@
+﻿string login = Console.ReadLine();
+
+char[] passowrd = login.Reverse().ToArray();
+string passwordString = new string(passowrd);
+int attempts = 1;
+
+while (true)
+{
+    string newPassword = Console.ReadLine();
+
+    if (passwordString == newPassword)
+    {
+        Console.WriteLine($"User {login} logged in. ");
+        return;
+    }
+    else
+    {
+        if (attempts == 4)
+        {
+            Console.WriteLine($"User {login} blocked!");
+            return;
+        }
+        Console.WriteLine("Incorrect password. Try again.");
+        attempts++;
+
+    }
+
+}
+
