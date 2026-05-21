8027dec7b556102d18fb62fec4731c6f9ac78f63
Boyan Venkov <boyanvenkov@yahoo.com>
2026-05-21 17:53:50 +0300

    Added a New Project

---
+++ 6. Strong Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+int input = int.Parse(Console.ReadLine());
+
+int currentNumber = 0;
+int factorial = 1;
+int sum = 0;
+
+string inputString = input.ToString();
+
+for (int i = inputString.Length - 1; i >= 0; i--)
+{
+    currentNumber = int.Parse(inputString[i].ToString());
+
+    if (currentNumber != 0)
+    {
+        for (int j = 1; j <= currentNumber; j++)
+        {
+            factorial *= j;
+
+        }
+    }
+    sum+=factorial;
+    factorial = 1;
+}
+if (sum == input)
+{
+    Console.WriteLine("yes");
+}
+else 
+{
+    Console.WriteLine("no");
+}
