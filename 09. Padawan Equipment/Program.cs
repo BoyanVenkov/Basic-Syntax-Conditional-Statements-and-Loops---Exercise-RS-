a1f682e4fae28d9a8971ee223112ea79298a9e29
Boyan Venkov <boyanvenkov@yahoo.com>
2026-06-20 08:17:19 +0300

    Added a New Project

---
+++ 09. Padawan Equipment/Program.cs
@@ -0,0 +1,28 @@
+﻿double money = double.Parse(Console.ReadLine());
+
+int students = int.Parse(Console.ReadLine());
+
+double lightSaberPrice = double.Parse(Console.ReadLine());
+
+double robePrice = double.Parse(Console.ReadLine());
+
+double beltPrice = double.Parse(Console.ReadLine());
+
+
+double totalLightSaberPrce = lightSaberPrice * (students + Math.Ceiling(students * 0.10));
+
+double totalRobePrice = robePrice * students;
+
+double totalBeltPrice = beltPrice * (students - students / 6);
+
+double totalPrice = totalLightSaberPrce + totalRobePrice + totalBeltPrice;
+
+
+if (totalPrice <= money)
+{
+    Console.WriteLine($"The money is enough - it would cost {totalPrice:f2} USD.");
+}
+else
+{
+    Console.WriteLine($"John will need {totalPrice - money:f2} USD more.");
+}
