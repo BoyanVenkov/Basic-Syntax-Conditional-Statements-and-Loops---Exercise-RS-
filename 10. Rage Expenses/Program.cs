8a98695300aa5cf7321e81622495bf8a3da6b1dd
Boyan Venkov <boyanvenkov@yahoo.com>
2026-06-20 08:24:10 +0300

    Added a New Project

---
+++ 10. Rage Expenses/Program.cs
@@ -0,0 +1,22 @@
+﻿int lostGames = int.Parse(Console.ReadLine());
+
+double headsetPrice = double.Parse(Console.ReadLine());
+
+double mousePrice = double.Parse(Console.ReadLine());
+
+double keyboardPrice = double.Parse(Console.ReadLine());
+
+double displayPrice = double.Parse(Console.ReadLine());
+
+
+double totalHeadsetPrice = headsetPrice * (lostGames / 2);
+
+double totalMousePrice = mousePrice * (lostGames / 3);
+
+double totalKeyboardPrice = keyboardPrice * (lostGames / 6);
+
+double totalDisplayPrice = displayPrice * (lostGames / 12);
+
+double totalRageExpenses = totalHeadsetPrice + totalMousePrice + totalKeyboardPrice + totalDisplayPrice;
+
+Console.WriteLine($"Rage expenses: {totalRageExpenses:f2} USD");
