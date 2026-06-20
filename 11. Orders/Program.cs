744d1ea191a119acd3280831fb1f85a7061f01db
Boyan Venkov <boyanvenkov@yahoo.com>
2026-06-20 08:32:46 +0300

    Added a New Project

---
+++ 11. Orders/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+double totalPrice = 0;
+
+for (int i = 1; i <= n; i++)
+{ 
+    double price = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsules = int.Parse(Console.ReadLine());
+
+    double orderPrice = price * days * capsules;
+
+    totalPrice += orderPrice;
+    Console.WriteLine($"The price for the coffee is: ${orderPrice:f2}");
+
+}
+Console.WriteLine($"Total: ${totalPrice:f2}");
